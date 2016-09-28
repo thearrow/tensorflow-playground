603e02c24ca1e92fecb2eed5e7a944eac1d8dd32
Jake Klingler <thearrow3456@gmail.com>
2016-09-27 23:52:59 -0600

    expand test script

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 import tensorflow as tf
-import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
 
+
+# tensorflow hello world
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
-
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
+print('tensorflow hello world complete')
+
+# keras hello world
+model = Sequential()
+model.add(Dense(output_dim=64, input_dim=100))
+model.add(Activation("relu"))
+model.add(Dense(output_dim=10))
+model.add(Activation("softmax"))
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+print('keras hello world complete')
